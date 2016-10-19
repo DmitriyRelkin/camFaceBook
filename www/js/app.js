a972775637593ad9a4198897f78c7ef5097571ab
DmitriyRelkin <dmitrij.relkin@gmail.com>
2016-10-19 16:34:13 +0300

    style papa

--- www/js/app.js
+++ www/js/app.js
@@ -1,13 +1,13 @@
-// Ionic Starter App
+angular
+.module('starter', [
+  'ionic',
+  'starter.controllers',
+  'ngCordova',
+  'ngCordovaOauth'
+]);
 
-// angular.module is a global place for creating, registering and retrieving Angular modules
-// 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
-// the 2nd parameter is an array of 'requires'
-// 'starter.services' is found in services.js
-// 'starter.controllers' is found in controllers.js
-// angular.module('starter', ['ionic','starter.controllers','ngCordova','ngCordovaOauth'])
-angular.module('starter', ['ionic','starter.controllers','ngCordova','ngCordovaOauth'])
-
+angular
+.module('starter')
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
     // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
@@ -15,38 +15,35 @@ angular.module('starter', ['ionic','starter.controllers','ngCordova','ngCordovaO
     if (window.cordova && window.cordova.plugins && window.cordova.plugins.Keyboard) {
       cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
       cordova.plugins.Keyboard.disableScroll(true);
-
     }
     if (window.StatusBar) {
       // org.apache.cordova.statusbar required
       StatusBar.styleDefault();
     }
   });
-})
+});
 
-.config(function($stateProvider, $urlRouterProvider) {
-
-  // Ionic uses AngularUI Router which uses the concept of states
-  // Learn more here: https://github.com/angular-ui/ui-router
-  // Set up the various states which the app can be in.
-  // Each state's controller can be found in controllers.js
+angular
+.module('starter')
+.config(config);
+function config($stateProvider, $urlRouterProvider) {
+  $urlRouterProvider.otherwise('/tab/camera');
   $stateProvider
-
   // setup an abstract state for the tabs directive
     .state('tab', {
     url: '/tab',
     abstract: true,
-    templateUrl: 'templates/tabs.html'
+    templateUrl: 'templates/tabs.html',
+    controllerAs: 'vm'
   })
-
   // Each tab has its own nav history stack:
-
   .state('tab.camera', {
     url: '/camera',
     views: {
       'tab-camera': {
         templateUrl: 'templates/tab-camera.html',
-        controller: 'CameraCtrl'
+        controller: 'CameraCtrl',
+        controllerAs: 'vm'
       }
     }
   })
@@ -56,22 +53,21 @@ angular.module('starter', ['ionic','starter.controllers','ngCordova','ngCordovaO
     views: {
       'tab-social': {
         templateUrl: 'templates/tab-social.html',
-        controller: 'SocialCtrl'
+        controller: 'SocialCtrl',
+        controllerAs: 'vm'
       }
     }
   })
-  //
-  // .state('tab.profile', {
-  //   url: '/profile',
-  //   views: {
-  //     'tab-profile': {
-  //       templateUrl: 'templates/profile.html',
-  //       controller: 'ProfileCtrl'
-  //     }
-  //   }
-  // })
 
+  .state('tab.profile', {
+    url: '/profile',
+    views: {
+      'tab-profile': {
+        templateUrl: 'templates/profile.html',
+        controller: 'ProfileCtrl',
+        controllerAs: 'vm'
+      }
+    }
+  });
   // if none of the above states are matched, use this as the fallback
-  $urlRouterProvider.otherwise('/tab/camera');
-
-});
+};
--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,8 +1,15 @@
-angular.module('starter.controllers', [])
+angular
+.module('starter.controllers', []);
 
-.controller('CameraCtrl', function($scope, $cordovaCamera) {
+angular
+.module('starter.controllers')
+.controller('CameraCtrl', CameraCtrl);
+CameraCtrl.$inject = ['$scope', '$cordovaCamera']
+function CameraCtrl($scope, $cordovaCamera) {
 
-  $scope.takePhoto = function () {
+  var vm = this;
+
+  vm.takePhoto = function () {
     var options = {
       quality: 100,
       destinationType: Camera.DestinationType.DATA_URL,
@@ -15,14 +22,14 @@ angular.module('starter.controllers', [])
       saveToPhotoAlbum: false
     };
 
-      $cordovaCamera.getPicture(options).then(function (imageData) {
-          $scope.imgURI = "data:image/jpeg;base64," + imageData;
-      }, function (err) {
-          // An error occured. Show a message to the user
-      });
+    $cordovaCamera.getPicture(options).then(function (imageData) {
+        vm.imgURI = "data:image/jpeg;base64," + imageData;
+    }, function (err) {
+        // An error occured. Show a message to the user
+    });
   }
 
-  $scope.choosePhoto = function () {
+  vm.choosePhoto = function () {
     var options = {
       quality: 100,
       destinationType: Camera.DestinationType.DATA_URL,
@@ -36,43 +43,55 @@ angular.module('starter.controllers', [])
     };
 
     $cordovaCamera.getPicture(options).then(function (imageData) {
-        $scope.imgURI = "data:image/jpeg;base64," + imageData;
+        vm.imgURI = "data:image/jpeg;base64," + imageData;
     }, function (err) {
         // An error occured. Show a message to the user
     });
   }
-})
-.controller("SocialCtrl", function($scope, $cordovaOauth, $location, $window) {
+}
 
-    $scope.login = function() {
-        $cordovaOauth.facebook("321686544890415", ["email", "user_website", "user_location", "user_relationships"]).then(function(result) {
-          $window.localStorage.setItem.accessToken = result.access_token;
-          $location.path("/profile");
-        }, function(error) {
-          alert("There was a problem signing in!");
+angular
+.module('starter.controllers')
+.controller('SocialCtrl', SocialCtrl);
+SocialCtrl.$inject = ['$scope', '$cordovaOauth', '$location', '$window']
+function SocialCtrl($scope, $cordovaOauth, $location, $window) {
+
+  var vm = this;
+
+  vm.login = function() {
+    $cordovaOauth.facebook("321686544890415", ["email", "user_website", "user_location", "user_relationships"]).then(function(result) {
+      $window.localStorage.setItem.accessToken = result.access_token;
+      $location.path("/profile");
+    }, function(error) {
+      alert("There was a problem signing in!");
+      console.log(error);
+    });
+  };
+}
+
+angular
+.module('starter.controllers')
+.controller('ProfileCtrl', ProfileCtrl);
+ProfileCtrl.$inject = ['$scope', '$location', '$window', '$http']
+function ProfileCtrl($scope, $location, $window, $http) {
+  var vm = this;
+
+  vm.init = function() {
+    if($window.localStorage.hasOwnProperty("accessToken") === true) {
+      // https://graph.facebook.com/v2.2/me
+      $http.get("https://facebook.com/me", { params: { access_token: $window.localStorage.accessToken, fields: "id,name,gender,location,website,picture,relationship_status", format: "json" }}).then(function(result) {
+          vm.profileData = result.data;
+      }, function(error) {
+          alert("There was a problem getting your profile.  Check the logs for details.");
           console.log(error);
-        });
-    };
+      });
+    } else {
+      alert("Not signed in");
+      // $location.path("/login");
+    }
+  };
+}
 
-})
-//
-// .controller("ProfileCtrl", function($scope, $http, $location) {
-//
-//     $scope.init = function() {
-//         if(window.localStorage.hasOwnProperty("accessToken") === true) {
-//             $http.get("https://graph.facebook.com/v2.2/me", { params: { access_token: window.localStorage.accessToken, fields: "id,name,gender,location,website,picture,relationship_status", format: "json" }}).then(function(result) {
-//                 $scope.profileData = result.data;
-//             }, function(error) {
-//                 alert("There was a problem getting your profile.  Check the logs for details.");
-//                 console.log(error);
-//             });
-//         } else {
-//             alert("Not signed in");
-//             $location.path("/login");
-//         }
-//     };
-//
-// })
 // // .controller("FeedCtrl", function($scope, $http, $location) {
 // //
 // //     $scope.init = function() {
